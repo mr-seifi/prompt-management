b64322c7b6abd10e6bd102b8434a837304b06be6
Amin Seifi <aminseiifi@gmail.com>
2025-04-15 17:22:04 +0330

    fix(backend): decreased refresh token lifetime

--- backend/prompt_manager/settings.py
+++ backend/prompt_manager/settings.py
@@ -238,7 +238,7 @@ CORS_ALLOW_HEADERS = [
 # JWT settings
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=3),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
 }
